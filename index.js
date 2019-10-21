6bf33161822343b68d893d022f2e42e7572a2973
Michael Stifter <stifter@parkside.at>
2019-10-21 11:37:03 +0200

    feat: add styling and display of token contents

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const request = require("request");
-const jwt = require("jwt-simple");
+const fs = require("fs");
+const whiskers = require("whiskers");
+const util = require("./util");
 
 // use `dotenv` package to load app configuration to environment vars
 require("dotenv").config();
@@ -9,7 +11,7 @@ const config = {
   app: {
     port: process.env.APP_PORT,
     allowSelfSignedCerts: process.env.ALLOW_SELF_SIGNED_CERTS,
-    corsOrigin = process.env.CORS_ORIGIN
+    corsOrigin: process.env.CORS_ORIGIN
   },
   oauth: {
     redirectPath: process.env.OAUTH_REDIRECT_PATH,
@@ -34,10 +36,7 @@ const app = express();
 // allow CORS from specified resources
 if (config.app.corsOrigin) {
   app.use((req, res, next) => {
-    res.header(
-      "Access-Control-Allow-Origin",
-      config.app.corsOrigin
-    );
+    res.header("Access-Control-Allow-Origin", config.app.corsOrigin);
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
@@ -46,41 +45,63 @@ if (config.app.corsOrigin) {
   });
 }
 
+app.use("/public", express.static("public"));
+
 app.get(config.oauth.redirectPath, (req, res) => {
-  console.log(`received callback`, req.query);
+  if (req.query["ref"] === "estmk") {
+    console.log(`received ref callback`, req.query);
 
-  const { code } = req.query;
+    const { code } = req.query;
 
-  const formData = {
-    grant_type: "authorization_code",
-    client_id: config.keycloak.clientId,
-    redirect_uri: config.oauth.redirectUri,
-    scope: "openid profile User",
-    code
-  };
+    const formData = {
+      grant_type: "authorization_code",
+      client_id: config.keycloak.clientId,
+      redirect_uri: config.oauth.redirectUri,
+      scope: "openid profile User",
+      code
+    };
 
-  const requestConfig = {
-    url: tokenEndpoint,
-    form: formData,
-    agentOptions: {}
-  };
+    const requestConfig = {
+      url: tokenEndpoint,
+      form: formData,
+      agentOptions: {}
+    };
 
-  if (config.app.allowSelfSignedCerts === "yes") {
-    requestConfig.agentOptions["rejectUnauthorized"] = false;
-  }
-
-  request.post(requestConfig, (err, response, body) => {
-    if (err) {
-      console.error(err);
-      return;
+    if (config.app.allowSelfSignedCerts === "yes") {
+      requestConfig.agentOptions["rejectUnauthorized"] = false;
     }
 
-    const data = JSON.parse(body);
+    request.post(requestConfig, (err, response, body) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send(err);
+      }
 
-    console.log("got data", data);
+      const data = JSON.parse(body);
 
-    res.status(200).json(data);
-  });
+      fs.readFile("./public/index.html", "utf8", (err, fileData) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).send(err);
+        }
+
+        const renderedTemplate = whiskers.render(fileData, {
+          raw: util.getPrettyString(data),
+          accessToken:
+            util.getDecodedPrettyString(data.access_token) ||
+            "No access token received",
+          refreshToken:
+            util.getDecodedPrettyString(data.refresh_token) ||
+            "No refresh token received",
+          idToken:
+            util.getDecodedPrettyString(data.id_token) || "No ID token received"
+        });
+        return res.status(200).send(renderedTemplate);
+      });
+    });
+  } else {
+    res.status(200).send();
+  }
 });
 
 const port = config.app.port;
---
+++ util.js
@@ -0,0 +1,21 @@
+const jwt = require("jwt-simple");
+
+const getDecodedToken = encoded => jwt.decode(encoded, null, true);
+
+const getPrettyString = obj => {
+  if (!obj) {
+    return null;
+  }
+
+  return JSON.stringify(obj, null, 2);
+};
+
+const getDecodedPrettyString = obj => {
+  if (!obj) {
+    return null;
+  }
+
+  return JSON.stringify(getDecodedToken(obj), null, 2);
+};
+
+module.exports = { getDecodedToken, getDecodedPrettyString, getPrettyString };
